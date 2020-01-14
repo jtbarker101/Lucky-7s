fd422c4996efcddf2f2cdd84e947a7ec90cdd370
Jon Barker <jtbarker50@gmail.com>
2020-01-14 06:55:34 -0700

    Finished Lucky 3

--- Lucky 7s/Source.cpp
+++ Lucky 7s/Source.cpp
@@ -34,7 +34,7 @@ void luckyTwo() {
 	}
 	cout << "The sum is " << answer;
 }
-
+////////////////////////////////////////////////////////////////////////
 /*void luckyTwo() {
 	int number = 0, sum = 0;
 	while (number <= 50) {
@@ -53,8 +53,13 @@ void luckyTwo() {
 
 void luckyThree() {
 	//Write a program that calculates and prints the first 10 terms of the factorial sequence.
+	int step = 10, number, factor = 1;
+	for (number = 1; number <= step; number++) {
+		factor = factor * number;
+		cout << "the factor of " << number << " is " << factor << endl;
+	}
 }
-
+////////////////////////////////////////////////////////////////////////
 void luckyFour() {
 	//Write a program that reads in a string and determines whether it is a palindrome
 	char string1[20];
@@ -77,7 +82,7 @@ void luckyFour() {
 		cout << "this string is a palindrome.";
 	}
 }
-
+////////////////////////////////////////////////////////////////////////
 void luckyFive() {
 	int response, step = 1, test = 0, end = 0;
 	cout << "Please enter a number.    ";
@@ -97,12 +102,12 @@ void luckyFive() {
 		cout << "this number is not prime";
 	}
 }
-
+////////////////////////////////////////////////////////////////////////
 void luckySix() {
 	//Write a program that creates an array of integers using an initialization list 
 	//and calculates and displays the average of the values.
 }
-
+////////////////////////////////////////////////////////////////////////
 void luckySeven() {
 	/*Write a program that gets a positive integer n from the user and creates an array of n. 
 	The program should read strings from a file, fill the array with the first n strings, 
@@ -110,7 +115,7 @@ void luckySeven() {
 	the file is greater than
 	*/
 }
-
+////////////////////////////////////////////////////////////////////////
 int main() {
 	int lucky;
 	cout << "which Lucky do you want to test ";
